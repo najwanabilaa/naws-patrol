b3f2adb1ca2546db8a3946f523e32bd241992223
cikalkautsar <cikalkautsar@upi.edu>
2025-05-15 21:00:36 +0700

    done register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
-
+        <h1>Naw's Patrol Register</h1>
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
@@ -39,13 +39,26 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+<!-- Phone Number -->
+<div class="mt-4">
+    <x-input-label for="phone" :value="__('Phone Number')" />
+
+    <x-text-input id="phone" class="block mt-1 w-full"
+                  type="textr"
+                  name="phone"
+                  :value="old('phone')"
+                  required autocomplete="tel" />
+
+    <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+</div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('Join Us') }}
             </x-primary-button>
         </div>
     </form>
