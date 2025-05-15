53663190bfb550416191fc1f2ea974601f4bbe13
cikalkautsar <cikalkautsar@upi.edu>
2025-05-15 20:36:25 +0700

    masukin brezee buat auth

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,5 +8,11 @@
 </head>
 <body>
     <h1>Register</h1>
+    <form action ="/Join us" method="post">
+        <label for="name">Name : </label><br>
+        <input type="text" id="name" name="nama" required><br>
+
+        <label for="email">Email : </label><br>
+        <input type="email" id="email" name="email" required><br>
 </body>
 </html>
