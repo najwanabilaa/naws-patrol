09a58899f4136d71534a4c7418c23171d388467c
salwa1k2 <salwakhairunnisa125@gmail.com>
2025-06-09 19:16:51 +0700

    forgot password done, cek lagi tapi bisi ngarah kemana

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,39 @@
+<link rel="stylesheet" href="{{ asset('css/reset-password.css') }}">
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<div class="container">
+        <h2>Lupa Kata Sandi?</h2>
+        <div class="border-gradient">
+            <div class="form-box">
+                <div class="mb-4 text-sm text-gray-600">
+                    {{ __('Masukkan email Anda untuk menerima tautan reset password.') }}
+                </div>
+
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+
+                    <!-- Email Address -->
+                    <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <x-primary-button>
+                            {{ __('Kirim Tautan Reset') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+
+                <p class="link mt-4"><a href="{{ route('login') }}">Kembali ke login</a></p>
+            </div>
+        </div>
+
+        <div class="cat-container">
+            <img src="{{ asset('cat.png') }}" alt="Cats" class="cat-img" />
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
